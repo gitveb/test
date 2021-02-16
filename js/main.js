22bbfa3604650042aefaa155f9d89dbfc20fae6e
gitveb <52931341+gitveb@users.noreply.github.com>
2021-02-16 22:55:26 +0400

    Delete js directory

--- js/main.js
+++
@@ -1,6 +0,0 @@
-let menuBtn = document.querySelector('.menu-btn');
-let menu = document.querySelector('.navbar-block');
-menuBtn.addEventListener('click', function () {
-	menu.classList.toggle('navbar-block-active');
-	menuBtn.classList.toggle('menu-btn-active');
-});
